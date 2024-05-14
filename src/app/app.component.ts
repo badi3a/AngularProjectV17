a0aae2d4df330a8f663e2bcd7030e1ec9bde8048
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2024-05-14 09:53:49 +0100

    enhanced version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import {FooterComponent} from "./shared/footer/footer.component";
+import {DetailComponent} from "./album/detail/detail.component";
+import {ListProductComponent} from "./album/list-product/list-product.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent, ListProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
